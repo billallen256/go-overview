89c7a7b1b85a28f58370c0a6daaffd7c13492411
witchard <witchard@hotmail.co.uk>
2019-05-17 11:28:01 +0100

    Added example of sync.WaitGroup.

---
+++ waitgroup.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+
+// START MAIN OMIT
+func main() {
+	// Create a WaitGroup thats waiting for three tasks
+	wg := &sync.WaitGroup{}
+	wg.Add(3) // What happens if we change this?
+
+	log.Println("Start")
+
+	for i := 1; i < 4; i++ {
+		// Launch three tasks as goroutines
+		// This syntax is executing an anonymous function
+		go func(ammount int) {
+			// Sleep the required number of seconds and then signal we are done
+			seconds := time.Second * time.Duration(ammount)
+			time.Sleep(seconds)
+			log.Println("Hello after", seconds)
+			wg.Done()
+		}(i)
+	}
+
+	// Code will block here until wg.Done() has been called 3 times
+	wg.Wait() // What happens if we comment this?
+	log.Println("Done")
+}
+
+// END MAIN OMIT
